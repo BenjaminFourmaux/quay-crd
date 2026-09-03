2b7c8c68927e05e437d5f0e99cf056e4095e726d
Benjamin Fourmaux <49273769+benjaminfourmaux@users.noreply.github.com>
2026-09-03 19:59:56 +0200

    add organization crud service

--- internal/services/quay/organization_services.go
+++ internal/services/quay/organization_services.go
@@ -1,6 +1,116 @@
 package quay
 
-func (c *Client) CreateOrganization() error {
-	//TODO: to implement
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) CreateOrganization(orgToCreate *CreateOrganization) (Organization, error) {
+	// prepare body
+	body, err := json.Marshal(orgToCreate)
+	if err != nil {
+		return Organization{}, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, c.BaseURL+"/api/"+c.APIVersion+"/organization/", bytes.NewBuffer(body))
+	if err != nil {
+		return Organization{}, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return Organization{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return Organization{}, parseAPIError(resp)
+	} else {
+		// Get the created org
+		return c.GetOrganization(orgToCreate.Name) // Quay doesn't return the created object... (not really RESTful API...)
+	}
+}
+
+func (c *Client) GetOrganization(name string) (Organization, error) {
+	req, err := http.NewRequest(http.MethodGet, c.BaseURL+"/api/"+c.APIVersion+"/organization/"+name, nil)
+	if err != nil {
+		return Organization{}, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return Organization{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return Organization{}, parseAPIError(resp)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Organization{}, err
+	}
+
+	org, err := ParseOrganization(body)
+	if err != nil {
+		return Organization{}, err
+	}
+
+	return org, nil
+}
+
+func (c *Client) UpdateOrganization(orgname string, orgToUpdate *UpdateOrganization) (Organization, error) {
+	body, err := json.Marshal(orgToUpdate)
+	if err != nil {
+		return Organization{}, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, c.BaseURL+"/api/"+c.APIVersion+"/organization/"+orgname, bytes.NewBuffer(body))
+	if err != nil {
+		return Organization{}, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return Organization{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return Organization{}, parseAPIError(resp)
+	} else {
+		// Get the updated org
+		return c.GetOrganization(orgname) // Quay doesn't return the created object... (not really RESTful API...)
+	}
+}
+
+func (c *Client) DeleteOrganization(orgname string) error {
+	req, err := http.NewRequest(http.MethodDelete, c.BaseURL+"/api/"+c.APIVersion+"/organization/"+orgname, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent {
+		return parseAPIError(resp)
+	}
+
 	return nil
 }
---
+++ internal/services/quay/utils.go
@@ -0,0 +1,61 @@
+package quay
+
+import (
+	"encoding/json"
+	"sort"
+)
+
+func ParseOrganization(body []byte) (Organization, error) {
+	var rawOrg struct {
+		Name                string          `json:"name"`
+		Email               string          `json:"email,omitempty"`
+		Avatar              Avatar          `json:"avatar"`
+		IsAdmin             bool            `json:"is_admin"`
+		IsMember            bool            `json:"is_member"`
+		Teams               map[string]Team `json:"teams"`
+		OrderedTeams        []string        `json:"ordered_teams"`
+		InvoiceEmail        bool            `json:"invoice_email"`
+		InvoiceEmailAddress *string         `json:"invoice_email_address"`
+		IsFreeAccount       bool            `json:"is_free_account"`
+	}
+
+	if err := json.Unmarshal(body, &rawOrg); err != nil {
+		return Organization{}, err
+	}
+
+	org := Organization{
+		Name:                rawOrg.Name,
+		Email:               rawOrg.Email,
+		Avatar:              rawOrg.Avatar,
+		IsAdmin:             rawOrg.IsAdmin,
+		IsMember:            rawOrg.IsMember,
+		InvoiceEmail:        rawOrg.InvoiceEmail,
+		InvoiceEmailAddress: rawOrg.InvoiceEmailAddress,
+		IsFreeAccount:       rawOrg.IsFreeAccount,
+	}
+
+	org.Teams = make([]Team, 0, len(rawOrg.Teams))
+	seenTeams := make(map[string]struct{}, len(rawOrg.Teams))
+
+	for _, teamName := range rawOrg.OrderedTeams {
+		if team, ok := rawOrg.Teams[teamName]; ok {
+			org.Teams = append(org.Teams, team)
+			seenTeams[teamName] = struct{}{}
+		}
+	}
+
+	if len(org.Teams) < len(rawOrg.Teams) {
+		remaining := make([]string, 0, len(rawOrg.Teams)-len(org.Teams))
+		for teamName := range rawOrg.Teams {
+			if _, ok := seenTeams[teamName]; !ok {
+				remaining = append(remaining, teamName)
+			}
+		}
+		sort.Strings(remaining)
+		for _, teamName := range remaining {
+			org.Teams = append(org.Teams, rawOrg.Teams[teamName])
+		}
+	}
+
+	return org, nil
+}
